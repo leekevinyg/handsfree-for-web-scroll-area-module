70babe7fe3aa66a9c9cb0e71470cffffaaa947af
klee2010 <klee2010@gmail.com>
2019-10-29 11:32:29 -0700

    add element picker functionality

--- src/index.js
+++ src/index.js
@@ -1,30 +1,39 @@
-export default {
-  name: 'i18n-name',
-  description: 'i18n-description',
-  // TODO insert icon
-  contexts: [{
-    context: 'root',
-    commands: [{
-      name: 'i18n-command.scroll-area',
-      help: 'i18n-help.scroll-area',
-      action: () => {
-        // TODO show element picker
-      },
-      group: 'i18n-group.scroll-area',
-      switchToContext: 'scroll-area' // TODO configure scroll-area context and actions
-    }],
-    i18n: {
-      en: {
-        'group.scroll-area': 'Scroll area',
-        'command.scroll-area': 'Scroll area',
-        'help.scroll-area': 'Scrolls selected element down'
-      }
-    }
-  }],
-  i18n: {
-    en: {
-      name: 'Scroll area',
-      description: 'Commands for scrolling a particular element on a webpage'
-    }
+function scrollArea() { // eslint-disable-line
+  return {
+    name: 'Scroll area',
+    description: 'Allows you pick an area to scroll on the page',
+    contexts: [{
+      context: 'root',
+      commands: [{
+        name: 'scroll area',
+        action() {
+          return {
+            context: 'pickLabel',
+            labels: 'div',
+            selectedElementHandler: el => ({
+              selectedElement: el
+            })
+          }
+        },
+        group: 'scroll area',
+        help: 'scroll a particular area on the page'
+      }]
+    }, {
+      context: 'scroll-area',
+      commands: [{
+        name: 'up',
+        action: () => {},
+        group: 'Scroll Direction',
+        help: 'Scrolls a selected element up',
+        switchToContext: 'root'
+      }, {
+        name: 'down',
+        action: () => {},
+        group: 'Scroll Direction',
+        help: 'Scrolls a selected element down',
+        switchToContext: 'root'
+      }]
+    }]
   }
 }
+
